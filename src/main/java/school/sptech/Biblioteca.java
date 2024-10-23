52e1400b15bca8f57065eb1f71ff9ea8a79d4dc4
FilipeGuerreiro <filipe.guerreiro@sptech.school>
2024-10-23 19:20:37 -0300

    subindo realização da atividade

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,69 @@
 package school.sptech;
 
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Biblioteca {
+    private String nome;
+    private List<Livro> livros;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro){
+        if(livro != null && livro.getTitulo() != null && !livro.getTitulo().isEmpty() && !livro.getTitulo().isBlank()
+        && livro.getAutor() != null && !livro.getAutor().isEmpty() && !livro.getAutor().isBlank()
+        && livro.getDataPublicacao() != null){
+        this.livros.add(livro);
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo){
+        livros.removeIf(livro -> livro.getTitulo().equalsIgnoreCase(titulo));
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo){
+        if(titulo != null){
+           return livros.stream().filter(l -> l.getTitulo()
+                            .equalsIgnoreCase(titulo)).findAny()
+                            .orElse(null);
+        }
+        return null;
+    }
+
+    public Integer contarLivros(){
+        return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano){
+        LocalDate data = LocalDate.of(ano, 12, 31);
+        List<Livro> novaLista = new ArrayList<>();
+        livros.forEach(l -> {
+            if(l.getDataPublicacao().isBefore(data) || l.getDataPublicacao().isEqual(data)){
+                novaLista.add(l);
+            }
+        });
+        return novaLista;
+    }
+
+    public List<Livro> retornarTopCincoLivros(){
+        return livros.stream().sorted(Comparator.comparing(Livro::calcularMediaAvaliacoes).reversed())
+                .limit(5).toList();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,69 @@
 package school.sptech;
 
-public class Livro {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Livro {
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes;
+
+    public Livro() {
+        this.avaliacoes = new ArrayList<>();
+    }
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas){
+        Avaliacao novaAvaliacao = new Avaliacao();
+        if(descricao != null && !descricao.isEmpty() && !descricao.isBlank() && qtdEstrelas != null){
+            if (qtdEstrelas >= 0.0 && qtdEstrelas <= 5.0){
+                novaAvaliacao.setDescricao(descricao);
+                novaAvaliacao.setQtdEstrelas(qtdEstrelas);
+
+                avaliacoes.add(novaAvaliacao);
+            }
+        }
+    }
+
+    public Double calcularMediaAvaliacoes(){
+        return avaliacoes.stream().mapToDouble(Avaliacao::getQtdEstrelas).average().orElse(0.0);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public List<Avaliacao> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor: " + this.autor;
+    }
 }
